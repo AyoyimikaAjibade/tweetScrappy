ba165cf7b4e67a39b107e46900c60bcf0d5f4f62
Ayoyimika Ajibade <ajibadeayoyimika@gmail.com>
2021-03-14 02:15:23 +0100

    updated display of da

--- tweet/models.py
+++ tweet/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django.utils import timezone
 class Tweet(models.Model):
-    id = models.IntegerField(primary_key=True,unique=True)
+    id = models.IntegerField(primary_key=True)
     tip = models.TextField(max_length=245)
-    timeStamps = models.DateTimeField()
+    timeStamps = models.DateTimeField(default=timezone.now)
     author = models.CharField(max_length=10)
     mediaUrl = models.URLField(max_length=2545,blank=True,null=True)
     totalLike = models.IntegerField()
--- tweet/views.py
+++ tweet/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
+from django.db.utils import OperationalError, IntegrityError
 from .models import Tweet
 import os
 from dotenv import load_dotenv
 load_dotenv()
 import tweepy
 
-
-def getTweet(searchWord):
+def setApi():
     consumerKey = str(os.getenv('apiKey'))
     consumerSecret = str(os.getenv('apiSecretKey'))
     accessToken = str(os.getenv('accessToken'))
@@ -14,18 +14,17 @@ def getTweet(searchWord):
 
     auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
     auth.set_access_token(accessToken, accessTokenSecret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    return tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
+
+def getData():
+    api = setApi()
+    searchWord = '@python_tip'
     tweets = []
-    count = 1
-    sinceDate = '2017-02-01'
+    for tweet in tweepy.Cursor(api.user_timeline, screen_name=searchWord).items():
 
-    for tweet in tweepy.Cursor(api.user_timeline, count=100, screen_name=searchWord, since=sinceDate,
-                               exclude_replies=True, include_rts=False).items(12):
-        count += 1
         try:
             if 'media' in tweet.entities:
-                # print(dir(tweet))
                 for image in tweet.entities['media']:
                     head = ['id', 'text', 'createdAt', 'author', 'mediaUrl', 'totalLike', 'totalRetweet']
                     data = [tweet.id_str, tweet.text, tweet.created_at, tweet.author.screen_name,
@@ -34,32 +33,34 @@ def getTweet(searchWord):
             else:
                 head = ['id', 'text', 'createdAt', 'author', 'totalLike', 'totalRetweet']
                 data = [tweet.id_str, tweet.text, tweet.created_at, tweet.author.screen_name,
-                       tweet.favorite_count, tweet.retweet_count]
+                        tweet.favorite_count, tweet.retweet_count]
 
             tweets.append(data)
             tweetDict = {head[i]: data[i] for i in range(len(head))}
-            tweet = Tweet.objects.get_or_create(
-                id=tweetDict.get('id'),
-                tip=tweetDict.get('text'),
-                timeStamps=str(tweetDict.get('createdAt')),
-                author=tweetDict.get('author'),
-                mediaUrl=tweetDict.get('mediaUrl'),
-                totalLike=tweetDict.get('totalLike'),
-                totalRetweet=tweetDict.get('totalRetweet')
-            )
-            if tweet:
-                print('imported...')
-
-            else:
-                print('ERROR...')
+            try:
+                print('RETREIVING DATA FROM API...')
+                tweet = Tweet.objects.create(
+                    id=tweetDict.get('id'),
+                    tip=tweetDict.get('text'),
+                    timeStamps=tweetDict.get('createdAt'),
+                    author=tweetDict.get('author'),
+                    mediaUrl=tweetDict.get('mediaUrl'),
+                    totalLike=tweetDict.get('totalLike'),
+                    totalRetweet=tweetDict.get('totalRetweet')
+                )
+            except OperationalError as e:
+                print('ERROR', e)
+                continue
+            except IntegrityError as e:
+                print('ERROR', e)
+                continue
         except tweepy.TweepError as e:
             print('ERROR', e.reason)
             continue
-        return tweet
+    print('DATA SUCCESSFULLY RETREIVED')
 
-# word= '@python_tip'
-# context = getTweet(word)
-
+# Will schedule time to retrieve data at a certain time
+# getData()
 
 def showTweet(request):
     tweets = Tweet.objects.all().order_by('totalLike')
