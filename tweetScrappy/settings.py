f194ec6c372aa1d85dd9da6cab6a9917a9b53377
Ayoyimika Ajibade <ajibadeayoyimika@gmail.com>
2021-03-29 11:35:12 +0100

    'UPDATE'

--- tweetScrappy/settings.py
+++ tweetScrappy/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y81sa1#!ba=a(-#b_)gzd^8&s+b8t&d7gad5nadgqpxx(-2m%i'
+SECRET_KEY =str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
